8cd3364537a6c72bca83195317b44e6c636d87b1
BLACK MAFIA <100425878+Osadachami123@users.noreply.github.com>
2025-09-05 11:05:17 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const PREFIX = '.'
+export const OWNERS = ['94770458503@s.whatsapp.net']
+
+// Pair code system
+export const PAIR_CODE = 'OMdxzZCK#AIWrLyw0IlHjkviNYkUoZARLB0QqGP7OQNqEYG2JpE4'        // enter your session code here
+export const SESSION_FILE = `session-${PAIR_CODE}.json`
